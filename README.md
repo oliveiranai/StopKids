aa2e9c56fa50bfe57007ca59da132284989e9536
oliveiranai <naiane.br.oliveira@gmail.com>
2020-07-03 23:35:44 -0300

    Projeto adicionado

--- README.md
+++ README.md
@@ -7,5 +7,10 @@ Um dispostivo que possui um sensor reflexivo e um buzzer que emite um som sempre
 Neste sentido, foi desenvolvido um projeto de um dispositivo eletrônico com um sensor reflexivo que, ao detectar algo em sua frente é acionado um buzzer que emite um som.  Este alarme foi feito com a placa Wemos D1 R1 que possui o módulo wifi ESP8266-12E que envia e recebe o sinal via MQTT (Message Queue Telemetry Transport) que é um protocolo de comunicação amplamente utilizado em IoT (Internet of Things ou Internet das Coisas).
 
 ## Links
+[Alltime manual de exmplo led Wemos D1 ](http://iotapp.alltimetech.com.co/src/manuales/Wemos/CD/ESPCDLed_Uno/ESPCDLed_Uno.pdf)
+[Alltime manual de funcionameto Esp8266 - Wemos D1 ](http://iotapp.alltimetech.com.co/src/manuales/Wemos/1Dispositivo/Esp8266-Wemos%20D1.pdf)
+[Alltime Tech site](http://www.alltimetech.com.co/)
+[Simulação Tinkercad (apenas para visualização)](https://www.tinkercad.com/things/89OGOtTk1GM)
+[Fluxograma](https://www.heflo.com/)
 
-[Simulação Tinkercad](https://www.tinkercad.com/things/89OGOtTk1GM)
+
--- stopkids.ino
+++ stopkids.ino
@@ -1,37 +1,58 @@
+/*Realizado por  :Alltime Tech (www.alltimetech.com.co)
+
+  Descripcion : Con este programa podra manipular todos tus sensores y actuadores en conjunto con el mismo controlador , a continuacion el sistema genera automaticamenta cada una
+                de las variables y funciones para garantizar la conexion satisfactoria con la platafoma
+
+      Hardware necesario :
+
+      -Tarjeta Wemos R1 D1. https://www.prometec.net/wemos-d1-esp8266-wifi/ */
 
 //*******************************************************Configuracion del sistema****************************************************
 
 //-----------------------------------Declaracion de librerias--------------------------------
 
+//Nota liberias: en esta seccion se deben declarar cada una de las librerias teniendo en cuenta las instrucciones dadas en el manual en la seccion multiwidget
+
 #include "Wemosd1.h"
 Wemosd1_r1 Alltime;//instacia de la libreria
 
-//-----------------------------------Declaracion de variables--------------------------------
+//Aqui incluye tus librerias
 
+//-----------------------------------Aqui se declaran los pines-------------------------------
+
+
+//-----------------------------------Declaracion de variables--------------------------------
+/*Nota pines: en esta seccion se declararn los pines de los sensores y actuadores que controlara el sistema .se debe tener en cuenta que algunos pines especiales 
+              deben ser declarados de forma manual segun como dice el manual en la seccion multiwidget.*/
+
+// byte Outpins[1] = {D0}; byte Inpins[1] = {D2}; const char* susc[1] = {"Lbv"};
  
 const char* key = "ukxEV";const char* usuario = "irtSb";const char* pasw = "irtSb_iotalltime"; // datos de conexion con la plataforma
 
-char ssid[] = "AP 36"; char pass[] = "002002Gi"; // datos de conexion WIFI
+char ssid[] = "xxxxx"; char pass[] = "xxxxx"; // datos de conexion WIFI
+
+//nombre de los Widgets para enviar datos desde el controlador hacia la plataforma (Sensores)
+// apontador único e individual de conexão com widget sensor da plataforma
+ #define sensor  "Yj"
 
 
-
-#define swicht "Lbv" //apuntador de conexion a widget en la plataforma swicht 
-#define sensordata  "Yj" // apuntador de conexion a widget en la plataforma sensor
-
+//nombre de los Widgets para recibir datos desde la plataforma en el controlador (Actuadores)
+//apontador único e individual de conexão com widget alarme da plataforma  
+ #define Alarme  "Lbv"
 
 
 //------Pin de salida digital -------------------
 
-int Pin_reflexivo = D7; //Declaração de pinos do sensor 
-int buzzer = D3; //Declaração de entrada digital buzzer
+int Pin_reflexivo = D7; //Declaração do pino digital do sensor 
+int buzzer = D3; //Declaração do pino digital do buzzer
 
 //-----------------------------------Setup --------------------------------
 
 void setup() {
 
-  Alltime.setup (ssid, pass, key, usuario, pasw); //Rotina padrão de configuração da plataforma e conexão wifi ,  conexão mqtt
+  Alltime.setup (ssid, pass, key, usuario, pasw); //Rotina padrão de configuração da plataforma e conexão wifi , conexão mqtt
 
-  Alltime.suscripcion(swicht, buzzer); // subscriber mqtt e declaração de pino do buzze como saída
+  Alltime.suscripcion(Alarme, buzzer); // subscriber mqtt e declaração de pino do buzze como saída
   
   pinMode(Pin_reflexivo, INPUT);//Declaração como entrada do pino em que o sensor está conectado
 }
@@ -42,18 +63,17 @@ void setup() {
 
 void loop() {
       
-if (Alltime.Leer(swicht) == "1")//  envia 1 para ativar todo o sistema
+if (Alltime.Leer(Alarme) == "1")//  envia 1 para ativar todo o sistema
   {
     
-   if (digitalRead(Pin_reflexivo) == LOW) { // verifica o status do sensor e somente entra no "if" quendo altera seu estado para alto.
+   if (digitalRead(Pin_reflexivo) == LOW) { // verifica o status do sensor e entra no "if" quando altera seu estado para LOW.
     
-      digitalWrite(buzzer, HIGH); 
+      digitalWrite(buzzer, HIGH); //ativa o buzzer
 
-      Alltime.Enviar("1", sensordata); // envia o status do sensor para a plataforma ON
+      Alltime.Enviar("1", sensor); // publica "1" para status do sensor e torna ON na plataforma 
       delay (1000);//Tempo de espera
 
-      Alltime.Enviar("0", sensordata); //envia o status do sensor para a plataforma OFF
-
+      Alltime.Enviar("0", sensor; //publica "0" para status do sensor e torna Off na plataforma 
     }
     digitalWrite(buzzer, LOW); //desliga o buzzer
 }
